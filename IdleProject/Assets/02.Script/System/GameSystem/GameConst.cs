578ef5fbb5d25f6fff088a5c96ead91601d1d00c
Yeo <yeodaehyeon@gmail.com>
2025-04-07 11:05:35 +0900

    GameSystem 폴더 생성
    - GameConst / Enum 생성

---
+++ IdleProject/Assets/02.Script/System/GameSystem/GameConst.cs
@@ -0,0 +1,78 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameConst
+{
+    #region Data
+    public static Dictionary<long, string> LongToString;
+    public static Dictionary<string, long> StringToLong;
+    #endregion
+
+    #region Camera
+    public static Vector2 defaultResolution = new Vector2(1080, 1920);
+    #endregion
+
+    #region Path
+#if UNITY_EDITOR
+    public static readonly string CacheDirectoryPath = "Cache/";
+    public static readonly string LogDirectoryPath = CacheDirectoryPath + "/Log/";
+#else
+    public static readonly string CacheDirectoryPath = UnityEngine.Application.persistentDataPath + "/Cache/";
+    public static readonly string LogDirectoryPath = CacheDirectoryPath + "/Log/";
+#endif
+    #endregion
+
+    #region Enum Converter
+    public static Dictionary<string, eEffectChainCondition> EffectChainType;
+    public static Dictionary<string, eStatusType> StatusType;
+    public static Dictionary<string, eCalculateType> CalculateType;
+    public static Dictionary<string, eModifierType> ModifierType;
+    public static Dictionary<string, eItemType> ItemType;
+
+
+    public static void InitializeEnumConverter()
+    {
+        OnGenerateEnumContainer(ref EffectChainType);
+        OnGenerateEnumContainer(ref StatusType);
+        OnGenerateEnumContainer(ref CalculateType);
+        OnGenerateEnumContainer(ref ModifierType);
+        OnGenerateEnumContainer(ref ItemType);
+    }
+    public static void ClearEnumConverter()
+    {
+        //StringToStatusType = null;  
+    }
+    static void OnGenerateEnumContainer<T>(ref Dictionary<string, T> container) where T : Enum
+    {
+        Type type = typeof(T);
+        string[] names = Enum.GetNames(type);
+        Array values = Enum.GetValues(type);
+        container = new Dictionary<string, T>(names.Length);
+        int count = 0;
+        foreach (var element in values)
+            container.Add(names[count++], (T)element);
+    }
+    #endregion
+
+    public static void Initialize()
+    {
+        if (System.IO.Directory.Exists(CacheDirectoryPath) == false)
+            System.IO.Directory.CreateDirectory(CacheDirectoryPath);
+
+        StringToLong = new Dictionary<string, long>(200);
+        LongToString = new Dictionary<long, string>(200);
+        for (long i = 1; i < 201; ++i)
+        {
+            string key = i.ToString();
+            StringToLong.Add(key, i);
+            LongToString.Add(i, key);
+        }
+        InitializeEnumConverter();
+        //for(eStatusType i = 0; i < eStatusType.End; ++i)
+        //{
+        //    Sprite sprite = Resources.Load<Sprite>("");
+        //}
+    }
+}
---
+++ IdleProject/Assets/02.Script/System/GameSystem/GameEnum.cs
@@ -0,0 +1,245 @@
+
+#region Actor
+public enum eActorType:uint
+{
+    Character,
+    Enemy,
+}
+public enum eActorState
+{
+    None,
+    Spawn,
+    Active,
+    Death,
+    Clean,
+}
+public enum eComponent
+{
+    SkinComponent,
+    ControllerComponent,
+    StatComponent,
+    SkillComponent,
+}
+public enum eCharacterAnimState
+{
+    Idle ,
+    Move ,
+    Hit ,
+    Death ,
+}
+public enum eSkillState
+{
+    None,
+    Using,
+    Cooltime,
+    Inactive,
+}
+public enum eStatusType
+{
+    MaxHP,
+    Recovery,
+    Armor,
+    MoveSpeed,
+    Might,
+    ProjectileSpeed,
+    Duration,
+    Area,
+    CoolTime,
+    Amount,
+    Revival,
+    Magnet,
+    Luck,
+    Growth,
+    Greed,
+    Curse,
+}
+public enum eCalculateType
+{
+    Flat,
+    Percentage
+}
+public enum eModifierType
+{
+    Increase,
+    Decrease
+}
+public enum eAttachmentTarget
+{
+    OverHead,    
+    End,
+}
+#endregion
+
+#region Data
+[System.Flags]
+public enum eTableName
+{
+    LocalizingTable = 1 << 0,
+    CharacterTable = 1 << 1,
+    EnemyTable  =   1 << 2,
+    WaveTable = 1 << 3,
+    StageTable = 1 << 4,
+    SkillTable=1<<5,
+    EffectTable=1<<6,
+    StatusTable=1<<7,
+    ItemTable = 1 << 8,
+    EquipmentTable=1<<9,
+    All = ~0,
+}
+
+enum eAddressableState
+{
+    None,
+    Initialized,
+    FindPatch,
+    DownloadDependencies,
+    LoadMemory,
+    TableMemory,
+    Complete,
+}
+#endregion
+
+#region Localizing
+public enum eLanguage
+{
+    EN,     //English
+    KR,     //Korean
+    End
+}
+#endregion
+
+#region Effect
+public enum eEffectType
+{
+    Projectile,
+    Crash,
+    Shape,//To Do :추후 더 좋은 이름으로 변경 하자 
+}
+public enum eEffectChainCondition
+{
+    None,
+    Enable,
+    Disable,
+    Overlap,
+}
+[System.Flags]
+public enum eEffectAttribute
+{
+    Duration = 1 << 0,
+    Velocity = 1 << 1,
+    Overlap = 1 << 2,
+    PostEffect = 1 << 3,
+}
+public enum ePostEffectType
+{
+    None,
+    KnockBack,
+    Stun
+}
+#endregion
+
+#region Manager
+#endregion
+
+#region Pattern
+#endregion
+
+#region Scene
+#endregion
+
+#region Stage
+public enum eStageType
+{
+    Normal,
+    End
+}
+public enum eStageFrameworkState
+{
+    None,
+    SetUp,
+    InProgress,
+    Victory,
+    Defeat,
+    Cancel,
+    Clean,
+}
+#endregion
+
+#region System
+#region Background
+#endregion
+
+#region Camera
+public enum eCameraType
+    {
+        MainCamera,
+        VirtualCamera,
+    }
+#endregion
+
+#region Reposition
+#endregion
+
+#region Preference
+public enum ePreference
+{
+    BGM,
+    SFX,
+    Alram,
+    JoyStick,
+    Vibration,
+    Effect
+}
+#endregion
+#endregion
+
+#region Item
+public enum eItemType
+{
+    Gem,
+    Heart,
+}
+#endregion
+
+#region UI
+[System.Flags]//31 is Maximum Value
+public enum eUI
+{
+    Controller = 1 << 0,
+    BattleState = 1 << 1,
+    BattlePausePopUp = 1 << 2,
+    AlramPopUp = 1 << 3,
+    SettingPopUp=1<<4,
+    LevelUpPopUp=1<<5,
+    ResultPopUp = 1 << 6,
+    MenuButton=1<<7,
+    Lobby=1<<8,
+    PlayerInfo=1<<9,
+}
+public enum eMovableUIDir
+{
+    None,
+    LeftToRight,
+    RightToLeft,
+    TopToBottom,
+    BottomToTop
+}
+enum LetterBoxDirection
+{
+    Top,
+    Bottom,
+}
+public enum eFieldUI
+{
+    HPBar,
+    DamageText,
+}
+public enum eLobbyUI
+{
+    Store,
+    Temp1,
+    Battle,
+    Temp2,
+    Enforce,
+}
+#endregion
