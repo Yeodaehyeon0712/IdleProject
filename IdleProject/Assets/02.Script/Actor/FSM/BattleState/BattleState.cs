7c95aaeac5950d8a03c683f48972fc21415d87e8
Yeo <yeodaehyeon@gmail.com>
2025-04-09 01:09:16 +0900

    앞에 적이 없다면 넘어가게 처리함

--- IdleProject/Assets/02.Script/Actor/FSM/BattleState/BattleState.cs
+++ IdleProject/Assets/02.Script/Actor/FSM/BattleState/BattleState.cs
@@ -12,11 +12,46 @@ public class BattleState : BaseState
     }
     public override void OnStateStay(float deltaTime)
     {
+        var target = fsm.Target;
 
+        if (target != null)
+        {
+            if (target.FSMState == eFSMState.Death)
+            {
+                fsm.Target = null; return;
+            }
+
+
+
+        }
+        else
+        {
+            if (SearchTarget() == false)
+                fsm.State = eFSMState.Move;
+        }
     }
     public override void OnStateExit()
     {
 
     }
     #endregion
+
+    bool SearchTarget()
+    {
+        Actor actor = ActorManager.Instance.FindActor(fsm.ActorSearchingTool);
+        if (actor != null)
+        {
+            fsm.Target = actor;
+            return true;
+        }
+        return false;
+    }
+    void DefaultAttack()
+    {
+
+    }
+    void SkillAttack() 
+    { 
+
+    }
 }
