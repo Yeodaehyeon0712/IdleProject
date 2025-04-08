3cf90961fe079506398f01c0fec1ec338b16785d
Yeo <yeodaehyeon@gmail.com>
2025-04-08 10:14:07 +0900

    SnapShotDataProperty 생성
    - 저장되는 캐릭터의 값을 담는다

---
+++ IdleProject/Assets/02.Script/Data/DataClass/SnapShotDataProperty.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[System.Serializable]
+public class SnapShotData
+{
+    //능력치 작업 중이었음 . 
+    public Dictionary<eStatusType, int> statusLevelDic=new Dictionary<eStatusType, int>();
+    public int CurrentStageIndex;
+
+}
+//Have to Change Save In Server 
+public class SnapShotDataProperty : JsonSerializableData<SnapShotData, SnapShotDataProperty>
+{
+    protected override void SetDefaultValue()
+    {
+        data = new SnapShotData();
+        foreach(eStatusType a in System.Enum.GetValues(typeof(eStatusType)))
+        {
+            data.statusLevelDic[a] = 0;
+        }
+    }
+    public int GetStatusLevel(eStatusType type)
+    {
+        return data.statusLevelDic[type];
+    }
+    public int GetCurrentStage()
+    {
+        return data.CurrentStageIndex;
+    }
+}
