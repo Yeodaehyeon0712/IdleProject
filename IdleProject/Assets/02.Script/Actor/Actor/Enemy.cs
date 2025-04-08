4faa2620dd894bd5529bd96343595595bd3b8065
Yeo <yeodaehyeon@gmail.com>
2025-04-09 02:09:20 +0900

    Gold 주는 메서드 생성

--- IdleProject/Assets/02.Script/Actor/Actor/Enemy.cs
+++ IdleProject/Assets/02.Script/Actor/Actor/Enemy.cs
@@ -15,4 +15,15 @@ public class Enemy : Actor
         statusComponent = new EnemyStatusComponent(this);
         fsmComponent = new EnemyFSMComponent(this);
     }
+    public override void Death(float time = 2.5F)
+    {
+        base.Death(time);
+        DropGold();
+    }
+    void DropGold()
+    {
+        var snapShot = SnapShotDataProperty.Instance;
+        var gold = DataManager.StageTable[snapShot.GetCurrentStage()].GetGold();
+        snapShot.GetData.GoldAmount += gold;
+    }
 }
